a2c756b0370437277987950038ebe1153e5f638f
edwardmuss <you@example.com>
2024-04-19 11:06:17 +0300

    edit header

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -37,24 +37,7 @@
     <!-- twitter card ends from here -->
 
     <!--  FAVICON AND TOUCH ICONS -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/assets/img/favicon2.ico') }}"/>
-    <!-- this icon shows in browser toolbar -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('frontend/assets/img/favicon2.ico') }}"/>
-    <!-- this icon shows in browser toolbar -->
-    <!--<link rel="apple-touch-icon" sizes="57x57" href="{{ asset('frontend/assets/img/favicon/apple-icon-57x57.png') }}">-->
-    <!--<link rel="apple-touch-icon" sizes="60x60" href="{{ asset('frontend/assets/img/favicon/apple-icon-60x60.png') }}">-->
-    <!--<link rel="apple-touch-icon" sizes="72x72" href="{{ asset('frontend/assets/img/favicon/apple-icon-72x72.png') }}">-->
-    <!--<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('frontend/assets/img/favicon/apple-icon-76x76.png') }}">-->
-    <!--<link rel="apple-touch-icon" sizes="114x114" href="{{ asset('frontend/assets/img/favicon/apple-icon-114x114.png') }}">-->
-    <!--<link rel="apple-touch-icon" sizes="120x120" href="{{ asset('frontend/assets/img/favicon/apple-icon-120x120.png') }}">-->
-    <!--<link rel="apple-touch-icon" sizes="144x144" href="{{ asset('frontend/assets/img/favicon/apple-icon-144x144.png') }}">-->
-    <!--<link rel="apple-touch-icon" sizes="152x152" href="{{ asset('frontend/assets/img/favicon/apple-icon-152x152.png') }}">-->
-    <!--<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('frontend/assets/img/favicon/apple-icon-180x180.png') }}">-->
-    <!--<link rel="icon" type="image/png" sizes="192x192" href="{{ asset('frontend/assets/img/favicon/android-icon-192x192.png') }}">-->
-    <!--<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('frontend/assets/img/favicon/favicon-32x32.png') }}">-->
-    <!--<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('frontend/assets/img/favicon/favicon-96x96.png') }}">-->
-    <!--<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('frontend/assets/img/favicon/favicon-16x16.png') }}">-->
-    <link rel="icon" type="image/x-icon" href="{{ asset('frontend/assets/img/favicon2.ico') }}"/>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/assets/img/favicon.png') }}"/>
     <link rel="manifest" href="{{ asset('frontend/assets/img/favicon/manifest.json') }}">
 <!-- Font Awesome -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
